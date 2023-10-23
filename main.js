9c8f565223c4d1454bf24af7c7efb7adf3a7c94d
rosyad <rosyadblog@gmail.com>
2023-10-23 14:22:35 +0700

    main

---
+++ main.js
@@ -0,0 +1,9 @@
+onload = () =>{
+        document.body.classList.remove("container");
+};
+window.addEventListener('load', function() {
+        var audio = document.getElementById('myAudio');
+        audio.play();
+});
+
+// follow TIKTOK/IG: mhmdrosyad_
